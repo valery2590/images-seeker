89f48c5a95c6a064914aa28778ff5ab5e9e866d6
Valery FH <valeryfigueroah@gmail.com>
2021-01-05 15:29:55 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Buscador from "./components/Buscador/buscador";
+import Navigation from "./components/navigation/nav";
+import Resultado from "./components/resultado/resultado";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  };
+  paginaAnterior = () => {
+    console.log("anterior");
+  };
+
+  paginaSiguiente = () => {
+    //leer el state de la página actual
+    let pagina = this.state.pagina;
+    //sumar una pagina a la actual
+    pagina += 1; 
+    // agregar el cambio al state
+    this.setState({
+      pagina
+    })
+    console.log(pagina)
+  };
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${termino}`;
+    // console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((resultado) => this.setState({ imagenes: resultado.hits }));
+  };
+
+  datosBusqueda = (termino) => {
+    this.setState(
+      {
+        termino,
+      },
+      () => {
+        this.consultarApi();
+      }
+    );
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imágenes</p>
+
+          <Buscador datosBusqueda={this.datosBusqueda} />
+      
+        </div>
+        <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navigation/nav.js
@@ -0,0 +1,15 @@
+import './nav.css';
+
+const Navigation = () => {
+    return (
+        <div className="nav_container">
+            <ul className="nav_container_ul">
+                <li ><a className="nav_container_ul_li"> Intro</a></li>
+                <li ><a className="nav_container_ul_li"> Web</a></li>
+                <li ><a className="nav_container_ul_li"> Fotos</a></li>
+                <li ><a className="nav_container_ul_li"> Blog</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Navigation;
